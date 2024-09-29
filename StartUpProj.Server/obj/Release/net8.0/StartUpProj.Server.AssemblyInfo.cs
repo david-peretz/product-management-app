b6585d3f397ee331097055b115e08f98095e3d6a
david-peretz <david82761@gmail.com>
2024-09-29 11:51:50 +0300

    init

--- StartUpProj.Server/obj/Release/net8.0/StartUpProj.Server.AssemblyInfo.cs
+++ StartUpProj.Server/obj/Release/net8.0/StartUpProj.Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("StartUpProj.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf40c569d48f249e3f00ce22badaab4af500e6d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("StartUpProj.Server")]
